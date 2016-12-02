f1b6e2ad1da06c4f58e3d062d149391eca9bd61d
Luke <luke@Lucass-MacBook-Pro.local>
2016-12-01 22:59:42 -0600

    Favicon and test question layout

--- client/src/components/Test.js
+++ client/src/components/Test.js
@@ -9,9 +9,11 @@ const Test = React.createClass({
   render: function () {
     return (
       <div className='ui text container'>
-        <h1>Test</h1>
-        Question 1.
+        <h1>Question 1 of 18</h1>
+        <div className="ui divider"></div>
         <Table />
+        <div className="ui divider"></div>
+        Donec at interdum nisi, eu lacinia dui. Donec lorem tortor, tincidunt lacinia porta et, convallis eget arcu. Nam sit amet interdum justo. Morbi id iaculis nibh. Proin efficitur odio sem, nec sollicitudin ipsum faucibus non?
       </div>
     );
   },
