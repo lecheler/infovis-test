78bb8c88e4977aa47dbf860ed2ec2dd16a3b9f21
Luke <luke@Lucass-MacBook-Pro.local>
2016-12-01 21:06:54 -0600

    Route test

--- client/src/App.js
+++ client/src/App.js
@@ -1,62 +1,23 @@
 import React from 'react';
-import Directions from './components/Directions';
-import Start from './components/Start';
 import {
   Router,
   Route,
   browserHistory,
 } from 'react-router';
-
-// import SelectedFoods from './SelectedFoods';
-// import FoodSearch from './FoodSearch'; 
+import Directions from './components/Directions';
+import Start from './components/Start';
+import Test from './components/Test';
 
 const App = React.createClass({
-  getInitialState: function () {
-    return {
-      selectedFoods: [],
-    };
-  },
   render: function() {
     return (
       <Router history={browserHistory} >
         <Route path="/" component={Directions} />
         <Route path="/start" component={Start} />
+        <Route path="/test" component={Test} />
       </Router>
     );
   },
-  // render: function () {
-  //   return (
-  //     <div className='App'>
-  //       <div className='ui text container'>
-  //         <h1>Luke Test</h1>
-  //         <SelectedFoods
-  //           foods={this.state.selectedFoods}
-  //           onFoodClick={
-  //             (idx) => (
-  //               this.setState({
-  //                 selectedFoods: [
-  //                   ...this.state.selectedFoods.slice(0, idx),
-  //                   ...this.state.selectedFoods.slice(
-  //                     idx + 1, this.state.selectedFoods.length
-  //                   ),
-  //                 ],
-  //               })
-  //             )
-  //           }
-  //         />
-  //         <FoodSearch
-  //           onFoodClick={
-  //             (food) => (
-  //               this.setState({
-  //                 selectedFoods: this.state.selectedFoods.concat(food),
-  //               })
-  //             )
-  //           }
-  //         />
-  //       </div>
-  //     </div>
-  //   );
-  // },
 });
 
 export default App;
--- client/src/components/Test.js
+++ client/src/components/Test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {Rating} from 'react-semantify'; 
+// import Client from './Client';
 
-const Start = React.createClass({
+const Test = React.createClass({
   getInitialState: function () {
     return {
     };
@@ -9,12 +10,11 @@ const Start = React.createClass({
   render: function () {
     return (
       <div className='ui text container'>
-        <h1>Begin</h1>
-        enter code...
-        <Rating rating={1} maxRating={5} />
+        <h1>Test</h1>
+        Question 1.
       </div>
     );
   },
 });
 
-export default Start;
+export default Test;
